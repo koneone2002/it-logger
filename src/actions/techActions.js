1fb8cb40b73ce4ab1b378eeb0c761370af12f3f0
koneone2002 <katieo3507@gmail.com>
2020-06-04 21:23:11 -0700

    delete technician created

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -48,6 +48,25 @@ export const addTech = tech => async dispatch => {
     });
   }
 };
+// Delete tech
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
 
 // Set Loading to true
 export const setLoading = () => {
